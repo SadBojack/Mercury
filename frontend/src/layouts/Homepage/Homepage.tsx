59f865fe52179c02864b2ee0eb8336b4efe8bd29
SadBojack <139729186+SadBojack@users.noreply.github.com>
2025-10-20 04:14:38 +0000

    Practicing item tiles

--- frontend/src/layouts/Homepage/Homepage.tsx
+++ frontend/src/layouts/Homepage/Homepage.tsx
@@ -1,9 +1,37 @@
 export const Homepage = () => {
-    return (
-        <div>
-            <div>
-                Thinking
-            </div>
+  return (
+    <div className="row">
+      <div className="col">
+        <div className="card bg-secondary-subtle">
+          <img src="/public/vite.svg"  width="200" height="200" className="card-img-top" alt="..." />
+          <div className="card-img-overlay row">
+            <h5 className="col align-self-end card-title">200yen</h5>
+          </div>
         </div>
-    )
-}
+        <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card’s content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+      </div>
+      <div className="col">
+        <img src="/public/vite.svg" width="100" height="100" />
+        <h3>Hello World</h3>
+      </div>
+      <div className="col">
+        <img src="/public/vite.svg" width="100" height="100" />
+      </div>
+      <div className="col">
+        <img src="/public/vite.svg" width="100" height="100" />
+      </div>
+      <div className="col">
+        <img src="/public/vite.svg" width="100" height="100" />
+      </div>
+    </div>
+  );
+};
